a1c50b3a40bb179031eb9ccb2686b542e95b8806
Tiffany Price <tiffany.hood@stanfordalumni.org>
2016-10-10 19:29:59 -0700

    updating git

--- writeJsHere.js
+++ writeJsHere.js
@@ -1,1 +1,80 @@
+var youDontNeedToWorryAboutAnythingInHere = function() {
+  var NameArray = ['Noel Plain', 'Steffanie Plain', 'Hester Sanderfur', 'Retta Plain', 'Idella Saur','Shila Plain', "Marybeth Peavler", "Chadwick Jin", "Jerold Chauez", "Roselle Plain", "Doria Figgins", "Seth Arvizu", 'Gloria Wyche', "Melodi Plain", "Robin Shackleford", "Jack Plain", "Marina Prost", "Melvina Plain", "Ryan Plemons", "Janis Ohare", "Assata Shakur", "Kanye West", "Jay-Z", "Mariah Carey", "Bianca Gandolfo", "Cedric the Entertainer", "Chloe Plain"];
+  var JobArray = ['Mortician', 'Broadcaster', 'Craftsperson', 'Engineer', 'Interior designer', 'Nun'];
+  var CityArray = ["Scottsdale, Arizona", "Oakland, California", "Stockton, California", "New York, NY"];
+  var emptyNetwork = [];
+
+  function createNetwork(collection){
+
+    for (var i = 0; i < NameArray.length; i++) {
+      var randomJob = JobArray[Math.floor(Math.random() * JobArray.length)];
+      var randomCity = CityArray[Math.floor(Math.random() * CityArray.length)];
+
+      collection.push(createNewUser(NameArray[i], randomJob, randomCity));
+    }
+
+    return collection
+  }
+
+  return createNetwork(emptyNetwork);
+
+}
+
+function createNewUser(username, job, city){
+  var newUser = {
+    username: username,
+    job: job,
+    city: city,
+    friends: [],
+    family: [],
+    coworkers: [],
+  };
+
+  return newUser;
+
+};
+// creating the user you're going to be adding to
+var joeyProfile = createNewUser('Joey Plain', 'Engineer', 'Oakland, California');
+// creating the network collection you're going to be pulling
+var allUsers = youDontNeedToWorryAboutAnythingInHere();
+// check the console to see what these look like
+
+
+//console.dir(ourUser);
+//console.dir(userNetwork);
+console.dir(joeyProfile);
+console.dir(allUsers);
+
+
+for (var i=0; i < allUsers.length; i++) {
+var singleUser = allUsers[i];
+  if ( singleUser.city === joeyProfile.city) {
+    joeyProfile.friends.push(singleUser.username)
+  };
+  if ( singleUser.job === joeyProfile.job) {
+    joeyProfile.coworkers.push(singleUser.username)
+  };
+  var lastname = singleUser.username.split(" ")[1];
+  var joeyLastname = joeyProfile.username.split(" ")[1];
+  if (lastname === joeyLastname) {
+    joeyProfile.family.push(singleUser.username)
+  };
+
+for (var i=0; i < allUsers.family.length; i++) {
+  for (var i=0; i<allUsers.friends.length; i++) {
+    if (singleUser.family[i] === singleUser.friends[i]) {
+      console.log(singleUser.family[i] + 'equals' +singleUser.friends[i]);
+    }
+  }
+
+}
+
+};
+
+
+
+console.log(joeyProfile);
+
+
+
 //All your code goes here
